2380a603d0a816ad8258aca5769de08b991ea606
mtkwock <m.t.kwock@gmail.com>
2019-11-17 03:46:56 -0800

    Whitelisting necessary uncompiled background and injector files.

---
+++ bin/background.js
@@ -0,0 +1,24 @@
+chrome.runtime.onMessage.addListener((message, _sender, reply) => {
+    console.log(message);
+    chrome.tabs.captureVisibleTab(null, { format: 'png' }, (dataUri) => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        const img = new Image();
+        img.onload = () => {
+            const actualWidth = img.width;
+            const actualHeight = img.height;
+            canvas.width = actualWidth * (message.width / message.windowWidth);
+            canvas.height = actualHeight * (message.height / message.windowHeight);
+            context.drawImage(img, actualWidth * (message.offsetX / message.windowWidth), actualHeight * (message.offsetY / message.windowHeight));
+            const canvasData = canvas.toDataURL('image/png');
+            console.log(canvasData);
+            const link = document.createElement('a');
+            link.setAttribute('href', canvasData);
+            link.setAttribute('download', 'valeria-screenshot.png');
+            link.click();
+            reply('Finished');
+        };
+        img.src = dataUri;
+    });
+});
+//# sourceMappingURL=background.js.map
---
+++ bin/injector.js
@@ -0,0 +1,29 @@
+function loadScript(scriptName) {
+    const s = document.createElement('script');
+    s.src = chrome.runtime.getURL(scriptName);
+    s.onload = function () {
+        // this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
+}
+const screenShotter = document.createElement('button');
+screenShotter.id = 'idc-screenshot-button';
+screenShotter.style.display = 'none';
+screenShotter.innerText = 'Save Team Screenshot';
+screenShotter.onclick = () => {
+    const teamBuilder = document.getElementById('team-builder');
+    let rect = teamBuilder.getBoundingClientRect();
+    chrome.runtime.sendMessage({
+        width: rect.width,
+        height: rect.height,
+        offsetX: -1 * rect.left,
+        offsetY: -1 * rect.top,
+        windowHeight: window.innerHeight,
+        windowWidth: window.innerWidth,
+    }, (_response) => { });
+};
+document.body.appendChild(screenShotter);
+loadScript('bundle.js');
+// loadScript('src/templates.js');
+// loadScript('src/damage_ping.js');
+// loadScript('src/monster_instance.js');
